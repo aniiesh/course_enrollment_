a17b42b73458daa036da028a120df37e060ced5e
aniiesh <95913515+aniiesh@users.noreply.github.com>
2023-07-23 13:43:11 +0530

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Parttime.DAL;
+using System.Data.Entity.Infrastructure.Interception;
+
+namespace Parttime
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            DbInterception.Add(new UniversityInterceptorTransientErrors());
+            DbInterception.Add(new UniversityInterceptorLogging());
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# University-System
+This is University System.There some Login,Registration,Course,Student and Enrollment System
+Student can Enroll Some Courses.
+This is Samll Project
+
+#---Technology---#
+The Technology Used ASP.NET and MySql Database.
+And Also I used EntityFramework package in .NET
+It can be Create Database By using out ASP.NET CODE Module
+I Used MVC Architecture in ASP.NET
